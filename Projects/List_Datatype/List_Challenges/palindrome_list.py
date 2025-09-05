5eb1b22951ed78660fa7a36312e0a74058b29d63
Karim Ahmed <karim.ahmed14770@gmail.com>
2025-09-05 14:48:13 +0300

    Done added a solution

--- Projects/List_Datatype/List_Challenges/palindrome_list.py
+++ Projects/List_Datatype/List_Challenges/palindrome_list.py
@@ -8,6 +8,7 @@ def palindrome_check(L1):
     while counter<length:
         if L1[counter]!=L1[length-1-counter]:
             palindrome=False
+            break
         counter+=1
 
     return palindrome
@@ -18,3 +19,10 @@ L3=[1,2,4,5,6,7]
 print(palindrome_check(L1))
 print(palindrome_check(L2))
 print(palindrome_check(L3))
+
+
+#other solution
+L2=[10,30,50,80,90,90,80,50,30,10]
+rev=L2[::-1]
+if(rev==L2):
+    print("palindrome")
