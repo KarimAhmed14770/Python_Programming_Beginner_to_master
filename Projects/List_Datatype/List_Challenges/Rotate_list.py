7dd185ce2115f7cfa4f0da204353fb28e8f42852
Karim Ahmed <karim.ahmed14770@gmail.com>
2025-09-05 14:56:58 +0300

    Done

---
+++ Projects/List_Datatype/List_Challenges/Rotate_list.py
@@ -0,0 +1,13 @@
+#rotate a given list by n places
+n=2
+L1=[10,20,30,40,50,60,70]
+L2=[]
+counter=0
+
+while counter<n:
+    L2.append(L1[0])
+    L1.pop(0)
+    counter+=1
+
+L1.extend(L2)
+print(L1)
